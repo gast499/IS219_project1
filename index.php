0dd02f4cf1d8d3504b939016e69da79071c5f1fe
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2017-02-19 13:52:41 -0500

    Finished Education Section

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="Tameem Haj-Ibrahim">
 
-    <title>Bare - Start Bootstrap Template (for IS219)</title>
+    <title>Tameem Haj-Ibrahim</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -68,7 +68,65 @@
         </div>
         <!-- /.container-fluid -->
     </nav>
-
+    <!-- Header -->
+    
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="img/profile.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">Tameem Haj-Ibrahim</span>
+                        <hr class="star-light">
+                        <span class="skills">Web Developer - Programmer - Video Editor</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Education</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#highschool" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/cca.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#TU" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/TU.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#njit" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/njit.jpg" class="img-responsive" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- Page Content -->
     <div class="container">
 
@@ -86,7 +144,111 @@
 
     </div>
     <!-- /.container -->
-
+<!-- Portfolio Modals -->
+    <div class="portfolio-modal modal fade" id="highschool" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Commonwealth Connections Academy</h2>
+                            <hr class="star-primary">
+                            <img src="img/cca.png" class="img-responsive img-centered" alt="">
+                            <p>Attended a cyber charter high school.</p>
+                            <ul class="list-inline item-details">
+                                <li>School:
+                                    <strong><a href="http://www.connectionsacademy.com">Commonwealth Connections Academy</a>
+                                    </strong>
+                                </li>
+                                <li>Date:
+                                    <strong>September 2010 - June 2014</strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="TU" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Temple University</h2>
+                            <hr class="star-primary">
+                            <img src="img/TU.png" class="img-responsive img-centered" alt="">
+                            <p>Attended Temple University for a year before transferring to NJIT</p>
+                            <ul class="list-inline item-details">
+                                <li>School:
+                                    <strong><a href="https://www.temple.edu/">Temple University</a>
+                                    </strong>
+                                </li>
+                                <li>Date:
+                                    <strong>September 2014 - May 2015</strong>
+                                </li>
+                                <li>Area of Study:
+                                    <strong><a href="http://bulletin.temple.edu/undergraduate/science-technology/computer-information-science/mathematics-computer-science-bs/#requirementstext">Mathematics & Computer Science</a>
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="njit" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>New Jersey Institute of Technology</h2>
+                            <hr class="star-primary">
+                            <img src="img/njit.jpg" class="img-responsive img-centered" alt="">
+                            <p>Pursuing a Bachelor's Degree in Computer Science</p>
+                            <ul class="list-inline item-details">
+                                <li>School:
+                                    <strong><a href="http://www.njit.edu/">NJIT</a>
+                                    </strong>
+                                </li>
+                                <li>Date:
+                                    <strong>September 2015 - May 2018</strong>
+                                </li>
+                                <li>Area of Study:
+                                    <strong><a href="http://catalog.njit.edu/undergraduate/computing-sciences/computer-science/bs/">Computer Science</a>
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- jQuery Version 1.11.1 -->
     <script src="js/jquery.js"></script>
 
